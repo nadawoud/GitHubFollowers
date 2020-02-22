7720a8e67d061fb50347dd6b81cb743f82906a0a
Nada Yehia Dawoud <nadawoud@gmail.com>
2020-02-22 22:02:04 +0200

    Create custom UITextField

---
+++ GitHub Followers/GFTextField.swift
@@ -0,0 +1,41 @@
+//
+//  GFTextField.swift
+//  GitHub Followers
+//
+//  Created by Nada Yehia Dawoud on 2/22/20.
+//  Copyright © 2020 Mobile Apps Kitchen. All rights reserved.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false //To use AutoLayout
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        
+        placeholder = "Enter a username"
+    }
+}
