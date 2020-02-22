71c4a6e53090be717654c72d0fe5c0435d9cb19a
Nada Yehia Dawoud <nadawoud@gmail.com>
2020-02-22 18:00:43 +0200

    Create Search and FavoritesList ViewControllers

---
+++ GitHub Followers/FavoritesListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListViewController.swift
+//  GitHub Followers
+//
+//  Created by Nada Yehia Dawoud on 2/22/20.
+//  Copyright © 2020 Mobile Apps Kitchen. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ GitHub Followers/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  GitHub Followers
+//
+//  Created by Nada Yehia Dawoud on 2/22/20.
+//  Copyright © 2020 Mobile Apps Kitchen. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
