6037c91e90f68f4fd90285b267490aecb64d8def
Nada Yehia Dawoud <nadawoud@gmail.com>
2020-02-23 21:57:53 +0200

    Create two new custom labels

---
+++ GitHub Followers/GFBodyLablel.swift
@@ -0,0 +1,38 @@
+//
+//  GFBodyLablel.swift
+//  GitHub Followers
+//
+//  Created by Nada Yehia Dawoud on 2/23/20.
+//  Copyright © 2020 Mobile Apps Kitchen. All rights reserved.
+//
+
+import UIKit
+
+class GFBodyLablel: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    
+    private func configure() {
+        font = UIFont.preferredFont(forTextStyle: .body)
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
---
+++ GitHub Followers/GFTitleLabel.swift
@@ -0,0 +1,39 @@
+//
+//  GFTitleLabel.swift
+//  GitHub Followers
+//
+//  Created by Nada Yehia Dawoud on 2/23/20.
+//  Copyright © 2020 Mobile Apps Kitchen. All rights reserved.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        configure()
+    }
+    
+    
+    private func configure() {
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
