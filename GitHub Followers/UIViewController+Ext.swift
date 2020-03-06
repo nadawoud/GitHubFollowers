cbb836bda461ef5d3adf00d48e6c53d62b1798e8
Nada Yehia Dawoud <nadawoud@gmail.com>
2020-03-07 00:00:30 +0200

    Create UIViewController extension

---
+++ GitHub Followers/UIViewController+Ext.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+Ext.swift
+//  GitHub Followers
+//
+//  Created by Nada Yehia Dawoud on 3/6/20.
+//  Copyright © 2020 Mobile Apps Kitchen. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertViewController(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+}
