23c586fbae832bbd0103846f4ec0232261c6340c
Nada Yehia Dawoud <nadawoud@gmail.com>
2020-03-11 22:52:12 +0200

    Create Follower model

---
+++ GitHub Followers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  GitHub Followers
+//
+//  Created by Nada Yehia Dawoud on 3/11/20.
+//  Copyright © 2020 Mobile Apps Kitchen. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
