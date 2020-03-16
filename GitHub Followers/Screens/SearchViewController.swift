e53e0b5c87467344f5a6a5c71dc1b4c205c80535
Nada Yehia Dawoud <nadawoud@gmail.com>
2020-03-16 19:30:54 +0200

    Fix UINavigationBar bug when swiping  back to previous screen

--- GitHub Followers/Screens/SearchViewController.swift
+++ GitHub Followers/Screens/SearchViewController.swift
@@ -29,7 +29,7 @@ class SearchViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        navigationController?.isNavigationBarHidden = true
+        navigationController?.setNavigationBarHidden(true, animated: true)
     }
     
     
