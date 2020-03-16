9fde63dff0f91f626014c462ed315930f63be2e9
Nada Yehia Dawoud <nadawoud@gmail.com>
2020-03-17 00:49:33 +0200

    Create ErrorMessages enum

---
+++ GitHub Followers/Utilities/ErrorMessages.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorMessages.swift
+//  GitHub Followers
+//
+//  Created by Nada Yehia Dawoud on 3/17/20.
+//  Copyright © 2020 Mobile Apps Kitchen. All rights reserved.
+//
+
+import Foundation
+
+enum ErrorMessages: String {
+    case invalidUsername  = "Invalid username. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse  = "Invalid response from the server. Please try again."
+    case invalidData      = "Data recieved from the server was invalid. Please try again."
+}
